08e08bd595e0470f6b88a99de549cb8a85a01c79
eliseu441 <eliseu441@gmail.com>
2024-07-22 13:49:22 -0300

    fix aos repos

--- src/pages/Repos/Repos.jsx
+++ src/pages/Repos/Repos.jsx
@@ -63,7 +63,7 @@ function Repos() {
 
 
                     <div className='table-repos col-12'>
-                        <p className='ms-2' style={theme == false ? { fontWeight: 'bolder',color: 'white' } : { fontWeight: 'bolder',color: 'black' }}  data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">Search for an especific tech</p>
+                        <p className='ms-2' style={theme == false ? { fontWeight: 'bolder', color: 'white' } : { fontWeight: 'bolder', color: 'black' }} data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">Search for an especific tech</p>
                         <>
                             <Form.Control
                                 type="text"
@@ -111,54 +111,58 @@ function Repos() {
                             </div>
                         </div>
                     </div>
+                    <div class='experience-content' >
+
+
+                        <div class="row"   >
+                            <p style={{ textAlign: 'center' }} >
+                                <p class="project-subtitle" style={theme == false ? { color: 'white' } : { color: 'black' }}>tools that i have solid experience:</p>
+                                <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="60" height="60" /> </a>
+                                <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="aws" width="60" height="60" /> </a>
+                                <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="60" height="60" /> </a>
+                                <a href="https://www.rust-lang.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rust/rust-plain.svg" alt="rust" width="60" height="60" /> </a>
+                                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="60" height="60" /> </a>
+                                <a href="https://www.microsoft.com/en-us/sql-server" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg" alt="mssql" width="60" height="60" /> </a>
+                                <a href="https://dotnet.microsoft.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" alt="dotnet" width="60" height="60" /> </a>
+                                <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="60" height="60" /> </a>
+                                <a href="https://nestjs.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg" alt="nestjs" width="60" height="60" /> </a>
+                                <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="60" height="60" /> </a>
+                                <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="60" height="60" /> </a>
+                                <a href="https://www.jenkins.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg" alt="jenkins" width="60" height="60" /> </a>
+                                <a href="https://jestjs.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" alt="jest" width="60" height="60" /> </a>
+                                <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="60" height="60" /> </a>
+                                <a href="https://www.nginx.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="60" height="60" /> </a>
+                                <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="60" height="60" /> </a>
+                                <a href="https://www.oracle.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" alt="oracle" width="60" height="60" /> </a>
+                                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="60" height="60" /> </a>
+                                <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img class='icons' src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="60" height="60" /> </a>
+                                <a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg" alt="azure" width="60" height="60" /> </a>
+                                <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="60" height="60" /> </a>
+                                <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="60" height="60" /> </a>
+                                <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="60" height="60" /> </a>
+                                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="60" height="60" /> </a>
+                                <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="60" height="60" /> </a>
+                                <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="60" height="60" /> </a>
+                                <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="60" height="60" /> </a>
+                                <a href="https://www.adobe.com/in/products/illustrator.html" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg" alt="illustrator" width="60" height="60" /> </a>
+                                <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="60" height="60" /> </a>
+                                <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="60" height="60" /> </a>
+                                <a href="https://postman.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="60" height="60" /> </a>
+                                <a href="https://redis.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" alt="redis" width="60" height="60" /> </a>
+                                <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width="60" height="60" /> </a>
+                                <a href="https://www.chartjs.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.chartjs.org/media/logo-title.svg" alt="chartjs" width="60" height="60" /> </a>
+                                <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="60" height="60" /> </a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
-            <div class='experience-content' >
+            <div >
 
 
-                <div class=" row" >
-                    <p  data-aos="zoom-in" data-aos-duration="1000"  style={{ textAlign: 'center' }} >
-                        <p class="project-subtitle" style={theme == false ? { color: 'white' } : { color: 'black' }}>tools that i have solid experience:</p>
-                        <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="60" height="60" /> </a>
-                        <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="aws" width="60" height="60" /> </a>
-                        <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="60" height="60" /> </a>
-                        <a href="https://www.rust-lang.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rust/rust-plain.svg" alt="rust" width="60" height="60" /> </a>
-                        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="60" height="60" /> </a>
-                        <a href="https://www.microsoft.com/en-us/sql-server" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg" alt="mssql" width="60" height="60" /> </a>
-                        <a href="https://dotnet.microsoft.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" alt="dotnet" width="60" height="60" /> </a>
-                        <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="60" height="60" /> </a>
-                        <a href="https://nestjs.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg" alt="nestjs" width="60" height="60" /> </a>
-                        <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="60" height="60" /> </a>
-                        <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="60" height="60" /> </a>
-                        <a href="https://www.jenkins.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg" alt="jenkins" width="60" height="60" /> </a>
-                        <a href="https://jestjs.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" alt="jest" width="60" height="60" /> </a>
-                        <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="60" height="60" /> </a>
-                        <a href="https://www.nginx.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="60" height="60" /> </a>
-                        <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="60" height="60" /> </a>
-                        <a href="https://www.oracle.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" alt="oracle" width="60" height="60" /> </a>
-                        <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="60" height="60" /> </a>
-                        <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img class='icons' src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="60" height="60" /> </a>
-                        <a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg" alt="azure" width="60" height="60" /> </a>
-                        <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="60" height="60" /> </a>
-                        <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="60" height="60" /> </a>
-                        <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="60" height="60" /> </a>
-                        <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="60" height="60" /> </a>
-                        <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="60" height="60" /> </a>
-                        <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="60" height="60" /> </a>
-                        <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="60" height="60" /> </a>
-                        <a href="https://www.adobe.com/in/products/illustrator.html" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg" alt="illustrator" width="60" height="60" /> </a>
-                        <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="60" height="60" /> </a>
-                        <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="60" height="60" /> </a>
-                        <a href="https://postman.com" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="60" height="60" /> </a>
-                        <a href="https://redis.io" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" alt="redis" width="60" height="60" /> </a>
-                        <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width="60" height="60" /> </a>
-                        <a href="https://www.chartjs.org" target="_blank" rel="noreferrer"> <img class='icons' src="https://www.chartjs.org/media/logo-title.svg" alt="chartjs" width="60" height="60" /> </a>
-                        <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img class='icons' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="60" height="60" /> </a>
-                    </p>
-                </div>
+
             </div>
-
 
 
 
